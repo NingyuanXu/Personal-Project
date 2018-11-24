2408975aecc74a29610c76fc56dc91f11d517b2a
xuningyuan <xu.ningyuan@outlook.com>
2018-11-24 12:49:48 -0800

    P11 Final Version

--- src/main/ui/main.java
+++ src/main/ui/main.java
@@ -1,9 +1,12 @@
 package main.ui;
+
 public class main {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args)  {
         System.out.println("Welcome to the CPSC 210 Project for 2018W1!");
+
+
         new AssignemntTodoApp();
 
 
